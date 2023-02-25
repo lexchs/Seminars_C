ba2d609271eca07ec42f3257b2dfd9959c0d3e88
Alexey <alexcheremisin@mail.ru>
2023-02-25 23:33:16 +0300

    Homework 3,4

---
+++ Seminar_1/Homework_Sem_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Привет, введите первое число");
+int numa = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numb = int.Parse(Console.ReadLine());
+
+int max = numa;
+
+if(numa>numb)
+{
+   Console.WriteLine($"Число {numa} больше числа {numb}."); 
+}
+else
+{
+    Console.WriteLine($"Число {numb} больше числа {numa}.");
+}
---
+++ Seminar_1/Homework_Sem_2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Напиши 3 числа через пробел ");
+string[] arr= Console.ReadLine().Split();
+ 
+int numa = int.Parse(arr[0]);
+int numb = int.Parse(arr[1]);
+int numc = int.Parse(arr[2]);
+
+int max = numa;
+
+if(numa>numb)
+ {
+    if(numa>numc)
+    {
+        max =  numa;
+        Console.WriteLine($"{numa} - самое большое число");
+    }
+    else
+    {
+        max = numc;
+        Console.WriteLine($"{numc} - самое большое число");
+    }
+ }
+ else
+ {
+    if(numc>numb)
+    {
+        max = numc;
+        Console.WriteLine($"{numc} - самое большое число");
+    }
+    else
+    {
+       max = numb;
+        Console.WriteLine($"{numb} - самое большое число"); 
+    }
+ }
