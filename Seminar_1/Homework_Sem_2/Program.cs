365e01d5d452015a99c992faff9aa9bb0a23593b
Alexey <alexcheremisin@mail.ru>
2023-02-26 20:33:02 +0300

    finished work number 4

--- Seminar_1/Homework_Sem_2/Program.cs
+++ Seminar_1/Homework_Sem_2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Напиши 3 числа через пробел ");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Напиши 3 числа через пробел: ");
 string[] arr= Console.ReadLine().Split();
  
 int numa = int.Parse(arr[0]);
@@ -7,19 +9,11 @@ int numc = int.Parse(arr[2]);
 
 int max = numa;
 
-if(numa>numb)
- {
-    if(numa>numc)
-    {
-        max =  numa;
-        Console.WriteLine($"{numa} - самое большое число");
-    }
-    else
-    {
-        max = numc;
-        Console.WriteLine($"{numc} - самое большое число");
-    }
- }
+if (numa > numb & numa > numc)
+{
+    max = numa;
+    Console.WriteLine($"{numa} - самое большое число");
+}
  else
  {
     if(numc>numb)
