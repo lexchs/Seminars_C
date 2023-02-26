bd3490c4ded4a04898ea688f1a2cae9f8389440b
Alexey <alexcheremisin@mail.ru>
2023-02-26 20:32:12 +0300

    finished work number 8

---
+++ Seminar_1/Homework_Sem_4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+    
+for (int i = 2; i <= num; i += 2)
+    Console.Write($"{i} ");
