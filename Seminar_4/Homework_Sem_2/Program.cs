8b6642fd8f2cdf18a7c3ea90a88d28f9563abf0a
Alexey <alexcheremisin@mail.ru>
2023-03-07 19:16:21 +0300

    work_27

---
+++ Seminar_4/Homework_Sem_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 27: Напишите программу, которая 
+принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int AdditionOfNumbers(int a)
+{
+    int num = a;
+    int sum = 0;
+    int dig = 0;
+    while(num>0)
+    {
+        dig = num % 10;
+        num = num / 10;
+        sum = sum + dig;
+    }
+    return sum;
+}
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = AdditionOfNumbers(number);
+Console.Write(result);
