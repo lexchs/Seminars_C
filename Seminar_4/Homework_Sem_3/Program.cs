0912650955d971a18f4d19e885ebef7f9218d7c7
Alexey <alexcheremisin@mail.ru>
2023-03-07 19:16:48 +0300

    work_29

---
+++ Seminar_4/Homework_Sem_3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 29: Напишите программу, которая 
+задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] randmas = new int [8];
+for(int i = 0;i < randmas.Length; i++)
+{
+    randmas[i]= new Random().Next(-100,100);
+}
+
+void PrintArray(int [] arr)
+{
+    int count = 0;
+    while(count < arr.Length)
+    {
+        if(count == arr.Length - 1)
+        {
+            Console.Write($"{arr[count]}");
+            break;
+        }
+        Console.Write($"{arr[count]}, ");
+        count++;
+    }
+}
+
+PrintArray(randmas);
