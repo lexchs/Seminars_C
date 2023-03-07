e63121b282f30be0004b3bf7747e7f709a5e4e57
Alexey <alexcheremisin@mail.ru>
2023-03-07 18:45:37 +0300

    work_25

---
+++ Seminar_4/Homework_Sem_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 25: Напишите метод, который принимает на 
+вход два числа (A и B) и возводит число A 
+в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+double Exponentiation(int a, int b)
+{
+    double res = Math.Pow(a,b);
+    return res;
+}
+
+Console.Write("Enter number A: ");
+int numa = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number B: ");
+int numb = int.Parse(Console.ReadLine());
+
+double result = Exponentiation(numa, numb);
+Console.Write(result);
